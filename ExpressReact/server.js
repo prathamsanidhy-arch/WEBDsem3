14d4664fd3d35365901a6a05304bddb9d5411b5d
prathamsanidhy-arch <pratham.sanidhy@gmail.com>
2026-09-25 15:14:26 +0530

    expressreact

--- ExpressReact/server.js
+++ ExpressReact/server.js
@@ -1,23 +1,59 @@
-const express = require("express");
-const cors = require("cors");
+import express from "express";
+import fs from "fs";
+import cors from "cors";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("Express Server is running!");
+// GET products
+app.get("/api/products", (req, res) => {
+  const data = fs.readFileSync("products.json", "utf-8");
+
+  const products = JSON.parse(data);
+
+  res.json(products);
 });
 
-app.get("/api", (req, res) => {
-    res.json({
-        message: "Hello from Express Server"
-    });
+// POST product
+app.post("/api/products", (req, res) => {
+  const data = fs.readFileSync("products.json", "utf-8");
+
+  const products = JSON.parse(data);
+
+  const newProduct = {
+    id: products.length + 1,
+    name: req.body.name,
+    price: req.body.price,
+    category: req.body.category,
+  };
+
+  products.push(newProduct);
+
+  fs.writeFileSync("products.json", JSON.stringify(products, null, 2));
+
+  res.json(newProduct);
 });
 
-const PORT = 5000;
+// DELETE product
+app.delete("/api/products/:id", (req, res) => {
+  const data = fs.readFileSync("products.json", "utf-8");
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  let products = JSON.parse(data);
+
+  const id = parseInt(req.params.id);
+
+  products = products.filter((product) => product.id !== id);
+
+  fs.writeFileSync("products.json", JSON.stringify(products, null, 2));
+
+  res.json({
+    message: "Product deleted successfully",
+  });
 });
+
+app.listen(5000, () => {
+  console.log("Server running on http://localhost:5000");
+});
+paste this code in index.js
